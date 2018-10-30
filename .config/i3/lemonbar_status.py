7e0615bc1525a6672bd8c3fd625e8d4de4b2e0fa
Heng Teng Yi <randumbperson2003@gmail.com>
2018-10-30 21:53:21 +0800

    Add files via upload

--- .config/i3/lemonbar_status.py
+++ .config/i3/lemonbar_status.py
@@ -7,111 +7,137 @@ import socket
 import subprocess
 import psutil
 
-GEOMETRY = 'x36+0+0'
-FONT = 'DejaVu Sans Mono-19'
-BATTERY_DIR = '/sys/class/power_supply/BAT0'
-DELAY = 0.15
+# ---AESTHETICS---
+GEOMETRY = 'x38+0+0'
+FONT = 'GohuFont Nerd Font-11'
+FONT2 = 'Font Awesome 5 Free-11'
+DELAY = 0.2
 
-def format_fg(color):
-    return '%{F' + color + '}'
+# ---COLORS---
+FG_BODY_NORMAL = '#D8DEE9'
+FG_HEADER_NORMAL = '#FFFFFF'
 
-def format_bg(color):
-    return '%{B' + color + '}'
+BG_BODY_NORMAL = '#3B4252'
+BG_HEADER_NORMAL = '#3B4252'
 
-#Color for header text and body text
-FG_BODY_NORMAL = format_fg('#D8DEE9') #nord3
-FG_HEADER_NORMAL = format_fg('#FFFFFF') #white
+FG_LOW_BATTERY = '#FF616A'
+BG_LOW_BATTERY = '#3B4252'
 
-#Background color for body and background color for headers
-BG_BODY_NORMAL = format_bg('#2E3440') #nord0
-BG_HEADER_NORMAL = BG_BODY_NORMAL #same as body
+BG_BAR = '#3B4252'
 
-#Foreground and background color for all text
-FG_LOW_BATTERY = format_fg('#FF616A') #ALMOST nord11
-BG_LOW_BATTERY = format_bg('#2E3440') #nord0
+# ---ICONS---
+NOBATT = "\uf244"
+QUARTERBATT = "\uf243"
+HALFBATT = "\uf242"
+THREEQUARTERBATT = "\uf241"
+FULLBATT = "\uf240"
+CHARGING = "\uf0e7"
 
-#Background color for everything else
-BG_BAR = '#2E3440' #nord0
+NOVOL = "\uf026"
+LOWVOL = "\uf027"
+HIGHVOL = "\uf028"
+
+CONNECTED = '\uf1eb'
+NOTCONNECTED = "\uf06a"
+
+CALENDAR = "\uf073"
+CLOCK = "\uf017"
+
+# ---EVERYTHING ELSE---
+def format_fg(color): return '%{F' + color + '}'
+
+def format_bg(color): return '%{B' + color + '}'
+
+FG_BODY_NORMAL = format_fg(FG_BODY_NORMAL)
+FG_HEADER_NORMAL = format_fg(FG_HEADER_NORMAL)
+
+BG_BODY_NORMAL = format_bg(BG_BODY_NORMAL)
+BG_HEADER_NORMAL = format_bg(BG_HEADER_NORMAL)
+
+FG_LOW_BATTERY = format_fg(FG_LOW_BATTERY)
+BG_LOW_BATTERY = format_bg(BG_LOW_BATTERY)
 
 CENTER = '%{c}'
 LEFT = '%{l}'
 RIGHT = '%{r}'
-SEPARATOR = ' '
+SEPARATOR = '  '
+BATTERY_DIR = '/sys/class/power_supply/BAT0'
 cycle_num = 0
+
+Section = namedtuple('Section', ('alignment', 'fg_header', 'bg_header', 'header','fg_body', 'bg_body', 'body'))
 
 def get_stdout(command):
     pipe = subprocess.Popen(command.split(' '),
                             stdout=subprocess.PIPE,
                             stderr=subprocess.DEVNULL)
     return str(pipe.stdout.read())
-   
+
 def status_datetime():
     return datetime.now().strftime('%d/%m %H:%M')
 
 def status_volume():
-    volume_raw = get_stdout("amixer get Master")
-    return volume_raw.split('\\n')[-2].split(' ')[-2][1:-1]
+    return int(get_stdout("amixer get Master").split('\\n')[-2].split(' ')[-2][1:-2])
 
 def status_battery_level():
     with open(BATTERY_DIR + '/capacity') as f:
         return int(f.readlines()[0])
 
-def status_battery_status():
+def status_battery_status(lvl):
     with open(BATTERY_DIR + '/status') as f:
-        return 'D' if f.readlines()[0][0] == 'D' else '\u26A1'
+        if f.readlines()[0][0] == 'D':
+            if lvl <= 15:
+                return NOBATT
+            elif lvl <= 25:
+                return QUARTERBATT
+            elif lvl <= 50:
+                return HALFBATT
+            elif lvl <= 75:
+                return THREEQUARTERBATT
+            elif lvl <= 100:
+                return FULLBATT
+        else:
+            return CHARGING
 
 def status_connected():
     try:
         socket.create_connection(('www.google.com', 80))
     except OSError:
         return False
-    else:
-        return True
+    return True
 
-def status_network():
-    interfaces = get_stdout('iwconfig')
-    return interfaces.split('\\n')[0].split('ESSID:')[1].strip()[1:-1]
+def status_network(): 
+    return get_stdout('iwconfig').split('\\n')[0].split('ESSID:')[1].strip()[1:-1]
 
-def status_cpu():
-    return str(psutil.cpu_percent(interval=None))
-
-def status_ram():
-    return str(psutil.virtual_memory().percent)
-
-def status_disk():
-    return str(psutil.disk_usage('/').percent)
-
-def status_workspaces():
+def status_workspaces(low_batt):
     num_workspaces = int(get_stdout("xdotool get_num_desktops")[2:-3])
     cur_workspace = int(get_stdout("xdotool get_desktop")[2:-3])
 
     workspaces_str = ''
     for i in range(num_workspaces):
-        workspaces_str += '{}{}'.format(format_fg('#ffffff') if i == cur_workspace
-                                        else format_fg('#999999'), i+1)
+        if low_batt:
+            workspaces_str += '{}{}'.format(format_fg('#ff616a')
+                                            if i == cur_workspace else format_fg('#bf616a'), i+1)
+        else:
+            workspaces_str += '{}{}'.format(format_fg('#ffffff') if i == cur_workspace else format_fg('#999999'), i+1)
         if i < num_workspaces-1:
-			workspaces_str += '  '
+            workspaces_str += '  '
     workspaces_str += FG_BODY_NORMAL
-
     return workspaces_str
 
-Section = namedtuple('Section', ('alignment', 'fg_header', 'bg_header', 'header',
-                                 'fg_body', 'bg_body', 'body'))
 def sections_to_string(sections, separator):
     display_string = ''
-
     # Separate into left, center and right groups
     groups = groupby(sections, lambda s: s.alignment)
-    for k, sections in groups:
+    for sections in groups:
         group_string = format_bg(BG_BAR)
-        sections = [i for i in sections]
+        sections = [i for i in sections[1]]
 
         for idx, s in enumerate(sections):
             group_string += (s.alignment if idx == 0 else '') + \
-                            s.fg_header + s.bg_header + \
+                s.fg_header + s.bg_header + \
                             (' ' if s.header else '') + \
-                            s.header + ' ' + \
-                            s.fg_body + s.bg_body + s.body
+                s.header + ' ' + \
+                s.fg_body + s.bg_body + s.body
             if idx < len(sections) - 1:
                 group_string += separator
             display_string += group_string
@@ -128,30 +154,32 @@ def write_bar(bar, display_string):
 
 def run():
     global cycle_num
-    status = subprocess.Popen(['lemonbar', '-d', '-p', '-g', GEOMETRY,
-                               '-o', '1', '-f', FONT,
-                               '-B', BG_BAR],
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE)
+    status = subprocess.Popen(['lemonbar', '-f', FONT, '-f',
+                               FONT2, '-p', '-g', GEOMETRY,
+                               '-o', '1', '-B', BG_BAR],
+                              stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE)
     while True:
-        datetime = status_datetime()
+        datetime = status_datetime().split(' ')
+        datetime = CALENDAR + " " + \
+            datetime[0] + "  " + CLOCK + " " + datetime[1]
         volume = status_volume()
-		workspaces = status_workspaces()
-        cpu = status_cpu()
-        ram = status_ram()
-        disk = status_disk()
+        battery_level = status_battery_level()
+        battery_status = status_battery_status(battery_level)
+
+        low_battery = battery_level <= 15 and battery_status != CHARGING
+        high_volume = NOVOL if volume == 0 else (LOWVOL if volume <= 60 else HIGHVOL)
+        workspaces = status_workspaces(low_battery)
 
         # run expensive network operations only once in a while
-        if cycle_num % 30 == 0:
-            battery_level = status_battery_level()
+        if cycle_num % 40 == 0:
             connected = status_connected()
             if connected:
                 network = status_network()
             cycle_num = 0
 
-        battery_status = status_battery_status()
+        network_status = CONNECTED if connected else NOTCONNECTED
 
-        low_battery = battery_level <= 15 and battery_status == ' '
         fg_header = (FG_LOW_BATTERY if low_battery else FG_HEADER_NORMAL)
         bg_header = (BG_LOW_BATTERY if low_battery else BG_HEADER_NORMAL)
         fg_body = (FG_LOW_BATTERY if low_battery else FG_BODY_NORMAL)
@@ -159,16 +187,15 @@ def run():
 
         sections_status = [
             Section(LEFT, fg_header, bg_header,
-                    '', fg_body, bg_body, workspaces),
+                    str(workspaces), fg_body, bg_body, ''),
             Section(CENTER, fg_header, bg_header,
                     str(datetime), fg_body, bg_body, ''),
             Section(RIGHT, fg_header, bg_header,
-                    'VOL', fg_body, bg_body, str(volume)),
+                    high_volume, fg_body, bg_body, '{}%'.format(volume)),
             Section(RIGHT, fg_header, bg_header,
-                    'BATT', fg_body, bg_body, '{}% {}'.format(battery_level,
-                                                            battery_status)),
+                    battery_status, fg_body, bg_body, '{}%'.format(battery_level)),
             Section(RIGHT, fg_header, bg_header,
-                    'NET', fg_body, bg_body, (network if connected else '-'))
+                    network_status, fg_body, bg_body, (network if connected else '-'))
         ]
         display_string = sections_to_string(sections_status, SEPARATOR)
         write_bar(status, display_string)
@@ -176,5 +203,5 @@ def run():
         sleep(DELAY)
         cycle_num += 1
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     run()
